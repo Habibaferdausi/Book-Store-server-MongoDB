dda26cfb0106992e8a02a76ca934a2238f6b3060
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-14 22:20:22 +0600

    update,delete

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 require("dotenv").config();
 //middle
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ehup0wf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +23,45 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const bookCollection = client.db("book - store").collection("books");
+
+    app.post("/uploadbook", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await bookCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/uploadbook", async (req, res) => {
+      const book = bookCollection.find();
+      const result = await book.toArray();
+      res.send(result);
+    });
+
+    app.patch("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBookData = req.body;
+
+      const updateDoc = {
+        $set: {
+          ...updatedBookData,
+        },
+      };
+      const result = await bookCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    //delete
+
+    app.delete("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookCollection.deleteOne(filter);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
